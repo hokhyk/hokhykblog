1ec1d949e603ecaba44dfc32df99ae8d878a102c
hokhyk <hokhyk@gmail.com>
2019-08-20 08:20:47 +0000

    defining Administrator related routes.

--- routes/adminApi.php
+++ routes/adminApi.php
@@ -13,8 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
+// For Administrators' login.
+Route::post('/login', 'AdminAuthorization@Login');
 
-Route::apiResource('/manageusers', 'ManageUsersController');
 
-Route::apiResource('/administrator', 'AdministratorsController');
+// The following endpoints should be protected  before successful authentication.
+Route::group(['middleware' => ['admin_auth:admin_api']], function () {
 
+    //    Logout
+    Route::delete('/logout', 'AdminAuthorization@adminLogout');
+
+
+    // For administrators to manage administrators in App\Entities\AdminUser Model.
+    Route::apiResource('/administrator', 'AdministratorsController');
+
+
+    // For administrators to manage normal users in App\Entities\User Model.
+    Route::apiResource('/manageusers', 'ManageUsersController');
+
+
+    //TODO: get Administrator operation logs.
+   //      Route::get('/admin_user_logs', 'AdminOperationLog@index');
+});
