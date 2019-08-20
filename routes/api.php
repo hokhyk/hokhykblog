aec634175828387f6c5ec3ee0fe07cce07f20d2b
hokhyk <hokhyk@gmail.com>
2019-08-20 08:36:29 +0000

    rearrange routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// For all users to see blogs in the system.
 Route::group(['namespace' => 'Blog'], function() {
-    Route::apiResource('/articles', 'ArticlesController');
+
+    Route::apiResource('/articles', 'ArticlesController')->except(['articles.store', 'articles.update', 'articles.destroy']);
+
 });
 
 
-Route::group(['namespace' => 'User'], function() {
+// For Normal users to login.
+Route::post('/login', 'UserAuthorization@Login');
+
+
+
+// The following endpoints should be protected  before successful authentication.
+Route::group(['namespace' => 'User', 'middleware' => 'auth:api'], function() {
+
+    //    Logout
+    Route::delete('/logout', 'AdminAuthorization@adminLogout');
 
     // For Users to view or modify his own information.
     Route::get('/users/{id}', 'UsersController@showUserInfo')->name('showUser');
@@ -31,11 +39,15 @@ Route::group(['namespace' => 'User'], function() {
 
 
 
-    // For users to view a article's detail.
+    // For users to view an article's detail.
     Route::get('/users/{user_id}/articles/{article_id}', 'UsersController@showOneArticle')->name('showOneUserArticle');
 
     // For users to view someone's articles.
     Route::get('/users/{user_id}/articles', 'UsersController@showArticles')->name('showUserArticles');
+
+
+    // Allow authors and administrators to create or update articles.
+    Route::apiResource('/articles', 'ArticlesController')->except(['articles.index', 'articles.show']);
 
 
 });
---
+++ tests/Feature/AdministratorLoginTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Entities\AdminUser;
+
+
+class UserLoginTest extends TestCase
+{
+    use withFaker;
+
+
+    function
+}
